f13cd015e899c7a7688bf5f0181c88c655f12b45
Jiang Yin <jiangyin@gameframework.cn>
2018-05-14 15:39:53 +0800

    更新版本号

--- Scripts/Runtime/Base/GameEntry.cs
+++ Scripts/Runtime/Base/GameEntry.cs
@@ -21,7 +21,7 @@ namespace UnityGameFramework.Runtime
     /// </summary>
     public static class GameEntry
     {
-        private const string UnityGameFrameworkVersion = "3.1.2";
+        private const string UnityGameFrameworkVersion = "3.1.3";
         private static readonly LinkedList<GameFrameworkComponent> s_GameFrameworkComponents = new LinkedList<GameFrameworkComponent>();
 
         /// <summary>

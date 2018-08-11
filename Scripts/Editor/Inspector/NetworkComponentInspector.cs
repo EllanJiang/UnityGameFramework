f0a70a5254842d6876d5eb4e7c49b7a9d8be2181
Jiang Yin <jiangyin@gameframework.cn>
2018-08-11 16:31:03 +0800

    修改提示

--- Scripts/Editor/Inspector/NetworkComponentInspector.cs
+++ Scripts/Editor/Inspector/NetworkComponentInspector.cs
@@ -53,8 +53,8 @@ namespace UnityGameFramework.Editor
             {
                 EditorGUILayout.LabelField(networkChannel.Name, networkChannel.Connected ? "Connected" : "Disconnected");
                 EditorGUILayout.LabelField("Network Type", networkChannel.NetworkType.ToString());
-                EditorGUILayout.LabelField("Local Address", networkChannel.Connected ? Utility.Text.Format("{0}:{1}", networkChannel.LocalIPAddress.ToString(), networkChannel.LocalPort.ToString()) : "Unknown");
-                EditorGUILayout.LabelField("Remote Address", networkChannel.Connected ? Utility.Text.Format("{0}:{1}", networkChannel.RemoteIPAddress.ToString(), networkChannel.RemotePort.ToString()) : "Unknown");
+                EditorGUILayout.LabelField("Local Address", networkChannel.Connected ? Utility.Text.Format("{0}:{1}", networkChannel.LocalIPAddress.ToString(), networkChannel.LocalPort.ToString()) : "Unavailable");
+                EditorGUILayout.LabelField("Remote Address", networkChannel.Connected ? Utility.Text.Format("{0}:{1}", networkChannel.RemoteIPAddress.ToString(), networkChannel.RemotePort.ToString()) : "Unavailable");
                 EditorGUILayout.LabelField("Send Packet Count", networkChannel.SendPacketCount.ToString());
                 EditorGUILayout.LabelField("Receive Packet Count", networkChannel.ReceivePacketCount.ToString());
                 EditorGUILayout.LabelField("Heart Beat Interval", networkChannel.HeartBeatInterval.ToString());

469eca8e2d852ac8f088fd1a4fb77d89a0865014
Jiang Yin <jiangyin@gameframework.cn>
2019-04-16 14:58:29 +0800

    Object Pool 增加导出数据按钮

--- Scripts/Editor/Inspector/ObjectPoolComponentInspector.cs
+++ Scripts/Editor/Inspector/ObjectPoolComponentInspector.cs
@@ -7,7 +7,10 @@
 
 using GameFramework;
 using GameFramework.ObjectPool;
+using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Text;
 using UnityEditor;
 using UnityEngine;
 using UnityGameFramework.Runtime;
@@ -84,6 +87,28 @@ namespace UnityGameFramework.Editor
                         foreach (ObjectInfo objectInfo in objectInfos)
                         {
                             EditorGUILayout.LabelField(objectInfo.Name, Utility.Text.Format("{0}, {1}, {2}, {3}, {4}", objectInfo.Locked.ToString(), objectPool.AllowMultiSpawn ? objectInfo.SpawnCount.ToString() : objectInfo.IsInUse.ToString(), objectInfo.CustomCanReleaseFlag.ToString(), objectInfo.Priority.ToString(), objectInfo.LastUseTime.ToString("yyyy-MM-dd HH:mm:ss")));
+                        }
+
+                        if (GUILayout.Button("Export CSV Data"))
+                        {
+                            string exportFileName = EditorUtility.SaveFilePanel("Export CSV Data", string.Empty, Utility.Text.Format("Object Pool Data - {0}.csv", objectPool.Name), string.Empty);
+                            try
+                            {
+                                int index = 0;
+                                string[] data = new string[objectInfos.Length + 1];
+                                data[index++] = Utility.Text.Format("Name,Locked,{0},Custom Can Release Flag,Priority,Last Use Time", objectPool.AllowMultiSpawn ? "Count" : "In Use");
+                                foreach (ObjectInfo objectInfo in objectInfos)
+                                {
+                                    data[index++] = Utility.Text.Format("{0},{1},{2},{3},{4},{5}", objectInfo.Name, objectInfo.Locked.ToString(), objectPool.AllowMultiSpawn ? objectInfo.SpawnCount.ToString() : objectInfo.IsInUse.ToString(), objectInfo.CustomCanReleaseFlag.ToString(), objectInfo.Priority.ToString(), objectInfo.LastUseTime.ToString("yyyy-MM-dd HH:mm:ss"));
+                                }
+
+                                File.WriteAllLines(exportFileName, data, Encoding.UTF8);
+                                Debug.Log(Utility.Text.Format("Export CSV data to '{0}' success.", exportFileName));
+                            }
+                            catch (Exception exception)
+                            {
+                                Debug.LogError(Utility.Text.Format("Export CSV data to '{0}' failure, exception is '{1}'.", exportFileName, exception.Message));
+                            }
                         }
                     }
                     else

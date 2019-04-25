afc6f7d834896105c1414dffec22a958a6d29e44
Jiang Yin <jiangyin@gameframework.cn>
2019-04-25 12:00:14 +0800

    Unity 2019 兼容性修改

--- Scripts/Runtime/Debugger/DebuggerComponent.GraphicsInformationWindow.cs
+++ Scripts/Runtime/Debugger/DebuggerComponent.GraphicsInformationWindow.cs
@@ -41,6 +41,9 @@ namespace UnityGameFramework.Runtime
                     DrawItem("Max Cubemap Size:", SystemInfo.maxCubemapSize.ToString());
                     DrawItem("Graphics UV Starts At Top:", SystemInfo.graphicsUVStartsAtTop.ToString());
 #endif
+#if UNITY_2019_1_OR_NEWER
+                    DrawItem("Min Constant Buffer Offset Alignment:", SystemInfo.minConstantBufferOffsetAlignment.ToString());
+#endif
 #if UNITY_2018_3_OR_NEWER
                     DrawItem("Has Hidden Surface Removal On GPU:", SystemInfo.hasHiddenSurfaceRemovalOnGPU.ToString());
                     DrawItem("Has Dynamic Uniform Array Indexing In Fragment Shaders:", SystemInfo.hasDynamicUniformArrayIndexingInFragmentShaders.ToString());
@@ -94,6 +97,9 @@ namespace UnityGameFramework.Runtime
 #endif
 #if UNITY_2018_3_OR_NEWER
                     DrawItem("Supports Separated Render Targets Blend:", SystemInfo.supportsSeparatedRenderTargetsBlend.ToString());
+#endif
+#if UNITY_2019_1_OR_NEWER
+                    DrawItem("Supports Set Constant Buffer:", SystemInfo.supportsSetConstantBuffer.ToString());
 #endif
                 }
                 GUILayout.EndVertical();

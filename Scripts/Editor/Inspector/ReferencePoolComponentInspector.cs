206b41c17f2576f2ffcc416630133b91e0c339a7
Jiang Yin <jiangyin@gameframework.cn>
2019-08-14 17:43:51 +0800

    增加 Reference Pool Debugger

--- Scripts/Editor/Inspector/ReferencePoolComponentInspector.cs
+++ Scripts/Editor/Inspector/ReferencePoolComponentInspector.cs
@@ -21,7 +21,7 @@ namespace UnityGameFramework.Editor
     {
         private readonly Dictionary<string, List<ReferencePoolInfo>> m_ReferencePoolInfos = new Dictionary<string, List<ReferencePoolInfo>>();
         private readonly HashSet<string> m_OpenedItems = new HashSet<string>();
-        private bool m_ShowFullName = false;
+        private bool m_ShowFullClassName = false;
 
         public override void OnInspectorGUI()
         {
@@ -38,7 +38,7 @@ namespace UnityGameFramework.Editor
             if (IsPrefabInHierarchy(t.gameObject))
             {
                 EditorGUILayout.LabelField("Reference Pool Count", ReferencePool.Count.ToString());
-                m_ShowFullName = EditorGUILayout.Toggle("Show Full Name", m_ShowFullName);
+                m_ShowFullClassName = EditorGUILayout.Toggle("Show Full Class Name", m_ShowFullClassName);
 
                 m_ReferencePoolInfos.Clear();
                 ReferencePoolInfo[] referencePoolInfos = ReferencePool.GetAllReferencePoolInfos();
@@ -90,7 +90,7 @@ namespace UnityGameFramework.Editor
                                     {
                                         int index = 0;
                                         string[] data = new string[assemblyReferencePoolInfo.Value.Count + 1];
-                                        data[index++] = "Name,FullName,Unused,Using,Acquire,Release,Add,Remove";
+                                        data[index++] = "Class Name,Full Class Name,Unused,Using,Acquire,Release,Add,Remove";
                                         foreach (ReferencePoolInfo referencePoolInfo in assemblyReferencePoolInfo.Value)
                                         {
                                             data[index++] = Utility.Text.Format("{0},{1},{2},{3},{4},{5},{6},{7}", referencePoolInfo.Type.Name, referencePoolInfo.Type.FullName, referencePoolInfo.UnusedReferenceCount.ToString(), referencePoolInfo.UsingReferenceCount.ToString(), referencePoolInfo.AcquireReferenceCount.ToString(), referencePoolInfo.ReleaseReferenceCount.ToString(), referencePoolInfo.AddReferenceCount.ToString(), referencePoolInfo.RemoveReferenceCount.ToString());
@@ -122,12 +122,12 @@ namespace UnityGameFramework.Editor
 
         private void DrawReferencePoolInfo(ReferencePoolInfo referencePoolInfo)
         {
-            EditorGUILayout.LabelField(m_ShowFullName ? referencePoolInfo.Type.FullName : referencePoolInfo.Type.Name, Utility.Text.Format("[Unused]{0} [Using]{1} [Acquire]{2} [Release]{3} [Add]{4} [Remove]{5}", referencePoolInfo.UnusedReferenceCount.ToString(), referencePoolInfo.UsingReferenceCount.ToString(), referencePoolInfo.AcquireReferenceCount.ToString(), referencePoolInfo.ReleaseReferenceCount.ToString(), referencePoolInfo.AddReferenceCount.ToString(), referencePoolInfo.RemoveReferenceCount.ToString()));
+            EditorGUILayout.LabelField(m_ShowFullClassName ? referencePoolInfo.Type.FullName : referencePoolInfo.Type.Name, Utility.Text.Format("[Unused]{0} [Using]{1} [Acquire]{2} [Release]{3} [Add]{4} [Remove]{5}", referencePoolInfo.UnusedReferenceCount.ToString(), referencePoolInfo.UsingReferenceCount.ToString(), referencePoolInfo.AcquireReferenceCount.ToString(), referencePoolInfo.ReleaseReferenceCount.ToString(), referencePoolInfo.AddReferenceCount.ToString(), referencePoolInfo.RemoveReferenceCount.ToString()));
         }
 
         private int Comparison(ReferencePoolInfo a, ReferencePoolInfo b)
         {
-            if (m_ShowFullName)
+            if (m_ShowFullClassName)
             {
                 return a.Type.FullName.CompareTo(b.Type.FullName);
             }
---
+++ Scripts/Runtime/Debugger/DebuggerComponent.ReferencePoolInformationWindow.cs
@@ -0,0 +1,112 @@
+﻿//------------------------------------------------------------
+// Game Framework
+// Copyright © 2013-2019 Jiang Yin. All rights reserved.
+// Homepage: http://gameframework.cn/
+// Feedback: mailto:jiangyin@gameframework.cn
+//------------------------------------------------------------
+
+using GameFramework;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnityGameFramework.Runtime
+{
+    public sealed partial class DebuggerComponent : GameFrameworkComponent
+    {
+        private sealed class ReferencePoolInformationWindow : ScrollableDebuggerWindowBase
+        {
+            private readonly Dictionary<string, List<ReferencePoolInfo>> m_ReferencePoolInfos = new Dictionary<string, List<ReferencePoolInfo>>();
+            private bool m_ShowFullClassName = false;
+
+            public override void Initialize(params object[] args)
+            {
+            }
+
+            protected override void OnDrawScrollableWindow()
+            {
+                GUILayout.Label("<b>Reference Pool Information</b>");
+                GUILayout.BeginVertical("box");
+                {
+                    DrawItem("Reference Pool Count", ReferencePool.Count.ToString());
+                }
+                GUILayout.EndVertical();
+
+                m_ShowFullClassName = GUILayout.Toggle(m_ShowFullClassName, "Show Full Class Name");
+                m_ReferencePoolInfos.Clear();
+                ReferencePoolInfo[] referencePoolInfos = ReferencePool.GetAllReferencePoolInfos();
+                foreach (ReferencePoolInfo referencePoolInfo in referencePoolInfos)
+                {
+                    string assemblyName = referencePoolInfo.Type.Assembly.GetName().Name;
+                    List<ReferencePoolInfo> results = null;
+                    if (!m_ReferencePoolInfos.TryGetValue(assemblyName, out results))
+                    {
+                        results = new List<ReferencePoolInfo>();
+                        m_ReferencePoolInfos.Add(assemblyName, results);
+                    }
+
+                    results.Add(referencePoolInfo);
+                }
+
+                foreach (KeyValuePair<string, List<ReferencePoolInfo>> assemblyReferencePoolInfo in m_ReferencePoolInfos)
+                {
+                    GUILayout.Label(Utility.Text.Format("<b>Assembly: {0}</b>", assemblyReferencePoolInfo.Key));
+                    GUILayout.BeginVertical("box");
+                    {
+                        GUILayout.BeginHorizontal();
+                        {
+                            GUILayout.Label(m_ShowFullClassName ? "<b>Full Class Name</b>" : "<b>Class Name</b>");
+                            GUILayout.Label("<b>Unused</b>", GUILayout.Width(60f));
+                            GUILayout.Label("<b>Using</b>", GUILayout.Width(60f));
+                            GUILayout.Label("<b>Acquire</b>", GUILayout.Width(60f));
+                            GUILayout.Label("<b>Release</b>", GUILayout.Width(60f));
+                            GUILayout.Label("<b>Add</b>", GUILayout.Width(60f));
+                            GUILayout.Label("<b>Remove</b>", GUILayout.Width(60f));
+                        }
+                        GUILayout.EndHorizontal();
+
+                        if (assemblyReferencePoolInfo.Value.Count > 0)
+                        {
+                            assemblyReferencePoolInfo.Value.Sort(Comparison);
+                            foreach (ReferencePoolInfo referencePoolInfo in assemblyReferencePoolInfo.Value)
+                            {
+                                DrawReferencePoolInfo(referencePoolInfo);
+                            }
+                        }
+                        else
+                        {
+                            GUILayout.Label("<i>Reference Pool is Empty ...</i>");
+                        }
+                    }
+                    GUILayout.EndVertical();
+                }
+            }
+
+            private void DrawReferencePoolInfo(ReferencePoolInfo referencePoolInfo)
+            {
+                GUILayout.BeginHorizontal();
+                {
+                    GUILayout.Label(m_ShowFullClassName ? referencePoolInfo.Type.FullName : referencePoolInfo.Type.Name);
+                    GUILayout.Label(referencePoolInfo.UnusedReferenceCount.ToString(), GUILayout.Width(60f));
+                    GUILayout.Label(referencePoolInfo.UsingReferenceCount.ToString(), GUILayout.Width(60f));
+                    GUILayout.Label(referencePoolInfo.AcquireReferenceCount.ToString(), GUILayout.Width(60f));
+                    GUILayout.Label(referencePoolInfo.ReleaseReferenceCount.ToString(), GUILayout.Width(60f));
+                    GUILayout.Label(referencePoolInfo.AddReferenceCount.ToString(), GUILayout.Width(60f));
+                    GUILayout.Label(referencePoolInfo.RemoveReferenceCount.ToString(), GUILayout.Width(60f));
+                }
+                GUILayout.EndHorizontal();
+            }
+
+            private int Comparison(ReferencePoolInfo a, ReferencePoolInfo b)
+            {
+                if (m_ShowFullClassName)
+                {
+                    return a.Type.FullName.CompareTo(b.Type.FullName);
+                }
+                else
+                {
+                    return a.Type.Name.CompareTo(b.Type.Name);
+                }
+            }
+        }
+    }
+}

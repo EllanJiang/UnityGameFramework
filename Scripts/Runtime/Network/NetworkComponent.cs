13c8f7a275c54097b84261a8553bb356c91ed4f4
Jiang Yin <jiangyin@gameframework.cn>
2019-11-15 00:52:17 +0800

    更新网络模块

--- Scripts/Runtime/Network/NetworkComponent.cs
+++ Scripts/Runtime/Network/NetworkComponent.cs
@@ -106,11 +106,12 @@ namespace UnityGameFramework.Runtime
         /// 创建网络频道。
         /// </summary>
         /// <param name="name">网络频道名称。</param>
+        /// <param name="serviceType">网络服务类型。</param>
         /// <param name="networkChannelHelper">网络频道辅助器。</param>
         /// <returns>要创建的网络频道。</returns>
-        public INetworkChannel CreateNetworkChannel(string name, INetworkChannelHelper networkChannelHelper)
+        public INetworkChannel CreateNetworkChannel(string name, ServiceType serviceType, INetworkChannelHelper networkChannelHelper)
         {
-            return m_NetworkManager.CreateNetworkChannel(name, networkChannelHelper);
+            return m_NetworkManager.CreateNetworkChannel(name, serviceType, networkChannelHelper);
         }
 
         /// <summary>

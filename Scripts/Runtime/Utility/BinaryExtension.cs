73eb83bd5d01a863a854f04d3ca652f12cf078b3
Jiang Yin <jiangyin@gameframework.cn>
2020-04-21 15:17:11 +0800

    增加对 BinaryReader 和 BinaryWriter 的扩展方法

--- Scripts/Runtime/Utility/BinaryExtension.cs
+++ Scripts/Runtime/Utility/BinaryExtension.cs
@@ -69,6 +69,98 @@ public static class BinaryExtension
     }
 
     /// <summary>
+    /// 从二进制流读取编码后的 32 位无符号整数。
+    /// </summary>
+    /// <param name="binaryReader">要读取的二进制流。</param>
+    /// <returns>读取的 32 位无符号整数。</returns>
+    public static uint Read7BitEncodedUInt32(this BinaryReader binaryReader)
+    {
+        return (uint)Read7BitEncodedInt32(binaryReader);
+    }
+
+    /// <summary>
+    /// 向二进制流写入编码后的 32 位无符号整数。
+    /// </summary>
+    /// <param name="binaryWriter">要写入的二进制流。</param>
+    /// <param name="value">要写入的 32 位无符号整数。</param>
+    public static void Write7BitEncodedUInt32(this BinaryWriter binaryWriter, uint value)
+    {
+        Write7BitEncodedInt32(binaryWriter, (int)value);
+    }
+
+    /// <summary>
+    /// 从二进制流读取编码后的 64 位有符号整数。
+    /// </summary>
+    /// <param name="binaryReader">要读取的二进制流。</param>
+    /// <returns>读取的 64 位有符号整数。</returns>
+    public static long Read7BitEncodedInt64(this BinaryReader binaryReader)
+    {
+        if (binaryReader == null)
+        {
+            throw new GameFrameworkException("Binary reader is invalid.");
+        }
+
+        long value = 0L;
+        int shift = 0;
+        byte b;
+        do
+        {
+            if (shift >= 70)
+            {
+                throw new GameFrameworkException("7 bit encoded int is invalid.");
+            }
+
+            b = binaryReader.ReadByte();
+            value |= (b & 0x7fL) << shift;
+            shift += 7;
+        } while ((b & 0x80) != 0);
+
+        return value;
+    }
+
+    /// <summary>
+    /// 向二进制流写入编码后的 64 位有符号整数。
+    /// </summary>
+    /// <param name="binaryWriter">要写入的二进制流。</param>
+    /// <param name="value">要写入的 64 位有符号整数。</param>
+    public static void Write7BitEncodedInt64(this BinaryWriter binaryWriter, long value)
+    {
+        if (binaryWriter == null)
+        {
+            throw new GameFrameworkException("Binary writer is invalid.");
+        }
+
+        ulong num = (ulong)value;
+        while (num >= 0x80)
+        {
+            binaryWriter.Write((byte)(num | 0x80));
+            num >>= 7;
+        }
+
+        binaryWriter.Write((byte)num);
+    }
+
+    /// <summary>
+    /// 从二进制流读取编码后的 64 位无符号整数。
+    /// </summary>
+    /// <param name="binaryReader">要读取的二进制流。</param>
+    /// <returns>读取的 64 位无符号整数。</returns>
+    public static ulong Read7BitEncodedUInt64(this BinaryReader binaryReader)
+    {
+        return (ulong)Read7BitEncodedInt64(binaryReader);
+    }
+
+    /// <summary>
+    /// 向二进制流写入编码后的 64 位无符号整数。
+    /// </summary>
+    /// <param name="binaryWriter">要写入的二进制流。</param>
+    /// <param name="value">要写入的 64 位无符号整数。</param>
+    public static void Write7BitEncodedUInt64(this BinaryWriter binaryWriter, ulong value)
+    {
+        Write7BitEncodedInt64(binaryWriter, (long)value);
+    }
+
+    /// <summary>
     /// 从二进制流读取加密字符串。
     /// </summary>
     /// <param name="binaryReader">要读取的二进制流。</param>

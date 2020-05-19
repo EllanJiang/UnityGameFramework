ff449639d51bead514dab63ac467b5546092f872
Jiang Yin <jiangyin@gameframework.cn>
2020-05-19 11:02:04 +0800

    更新序列化头格式

--- Scripts/Runtime/Setting/DefaultSettingSerializer.cs
+++ Scripts/Runtime/Setting/DefaultSettingSerializer.cs
@@ -14,7 +14,7 @@ namespace UnityGameFramework.Runtime
     /// </summary>
     public sealed class DefaultSettingSerializer : GameFrameworkSerializer<DefaultSetting>
     {
-        private static readonly byte[] Header = new byte[] { (byte)'E', (byte)'L', (byte)'S' };
+        private static readonly byte[] Header = new byte[] { (byte)'G', (byte)'F', (byte)'S' };
 
         /// <summary>
         /// 初始化默认游戏配置序列化器的新实例。

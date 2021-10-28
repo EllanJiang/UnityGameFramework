e7eb3ef11393df0ac504204c480cdb839752b05f
Jiang Yin <ellan@gameframework.cn>
2021-10-28 15:00:30 +0800

    修改注释

--- Scripts/Runtime/ReferencePool/ReferencePoolComponent.cs
+++ Scripts/Runtime/ReferencePool/ReferencePoolComponent.cs
@@ -11,7 +11,7 @@ using UnityEngine;
 namespace UnityGameFramework.Runtime
 {
     /// <summary>
-    /// 基础组件。
+    /// 引用池组件。
     /// </summary>
     [DisallowMultipleComponent]
     [AddComponentMenu("Game Framework/ReferencePool")]
